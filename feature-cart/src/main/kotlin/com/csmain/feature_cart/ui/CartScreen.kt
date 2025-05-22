ec707b51c444cc5e746c28bd8b81c18ea520a938
vtyric <misha.torno@gmail.com>
2025-05-22 16:43:10 +0500

    добавил переход в профиль из headerа

--- feature-cart/src/main/kotlin/com/csmain/feature_cart/ui/CartScreen.kt
+++ feature-cart/src/main/kotlin/com/csmain/feature_cart/ui/CartScreen.kt
@@ -1,16 +1,2 @@
 package com.csmain.feature_cart.ui
 
-import androidx.compose.runtime.Composable
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.csmain.common.models.Product
-
-@Composable
-fun CartScreen(
-    viewModel: CartViewModel =  hiltViewModel(),
-    onAddProduct: (product: Product) -> Unit,
-    onRemoveProduct: (id: String) -> Unit
-) {
-
-
-
-}
